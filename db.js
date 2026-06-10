024b9984c27037ac87fa1018f64f74515b769e43
fawzia 1995 <ramromio1995@gmail.com>
2026-06-10 11:22:41 +0300

    Fix sqlite database directory creation

--- db.js
+++ db.js
@@ -1,7 +1,13 @@
+const fs = require('fs');
 const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
 
-const dbFile = path.join(__dirname, 'db', 'events.db');
+const dbDir = path.join(__dirname, 'db');
+if (!fs.existsSync(dbDir)) {
+  fs.mkdirSync(dbDir, { recursive: true });
+}
+
+const dbFile = path.join(dbDir, 'events.db');
 const db = new sqlite3.Database(dbFile, (err) => {
   if (err) {
     console.error('Unable to open database:', err.message);
